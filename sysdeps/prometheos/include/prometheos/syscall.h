f8af2761f1b497d1fd96a4d115777f5807a64544
Valeryum999 <belenkov33@gmail.com>
2025-11-05 20:23:35 +0100

    added prometheos sysdeps

---
+++ sysdeps/prometheos/include/prometheos/syscall.h
@@ -0,0 +1,21 @@
+#ifndef _PROMETHEOS_SYSCALL_H
+#define _PROMETHEOS_SYSCALL_H
+
+#define restart_syscall 0
+#define exit 1
+#define fork 2
+#define read 3
+#define write 4
+#define open 5
+#define close 6
+#define waitpid 7
+#define creat 8
+#define link 9
+#define unlink 10
+#define execve 11
+#define chdir 12
+#define time 13
+#define mknod 14
+#define chmod 15
+
+#endif
